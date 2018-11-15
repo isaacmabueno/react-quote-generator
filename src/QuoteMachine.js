790090e769b74937adb91c2dc5d2d57fac0b4657
isaacmabueno <isaacmabueno@gmail.com>
2018-11-14 22:09:40 -0600

    rendering quote to dom on click

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -6,7 +6,11 @@ class QuoteMachine extends Component {
     constructor() {
         super();
         this.state = {
-            quote: '',
+            quote:  {
+                content: '',
+                link: '',
+                title: ''
+            },
             hasQuote: false
         }
         this.END_POINT = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
@@ -16,12 +20,26 @@ class QuoteMachine extends Component {
         fetch(this.END_POINT)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            if(data[0].content && data[0].title && data[0].link) {
+                let { quote } = this.state;
+                let quoteData = data[0];
+                quote.content = quoteData.content;
+                quote.link = quoteData.link;
+                quote.title = quoteData.title;
+                this.setState({ quote }, () => {
+                    if(this.state.hasQuote === false) {
+                        this.setState({ hasQuote: true })
+                    }
+                })
+            } 
+            else {
+                return console.error('no quote has been found');
+            }
         })
     }
 
     render() {
-        const { hasQuote } = this.state;
+        const { hasQuote, quote } = this.state;
         // console.log(this.state);
         return (
             <Fragment>
@@ -30,7 +48,7 @@ class QuoteMachine extends Component {
                 Push Me!
                 </Button>
                 <br/>
-                {hasQuote === true ? null : 'no quote yet'}
+                {hasQuote === true ? JSON.stringify(quote) : 'no quote yet'}
             </Fragment>
         )
     }
